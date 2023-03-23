5626147d5d273a100cf2899f54d3eee3d5f4271a
mathieumunyakazimoise <m.munyakazi1@alustudent.com>
2023-03-23 00:11:56 -0700

    10111

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -10,6 +10,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Intialize a new Rectangle.
+
         Args:
             width (int): The width of the new Rectangle.
             height (int): The height of the new Rectangle.
