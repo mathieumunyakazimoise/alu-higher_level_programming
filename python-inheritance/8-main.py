a08864e0ad2505f69ac61ad7a3a9bbcb45ea56de
mathieumunyakazimoise <m.munyakazi1@alustudent.com>
2023-03-22 23:48:59 -0700

    qdwfd

--- python-inheritance/8-main.py
+++
@@ -1,5 +0,0 @@
-<8-rectangle.Rectangle object at 0x7f6f488f7eb8>
-['_Rectangle__height', '_Rectangle__width', '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'area', 'integer_validator']
-[AttributeError] 'Rectangle' object has no attribute 'width'
-[TypeError] height must be an integer
-
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-Rectangle = __import__('8-rectangle').Rectangle
+# 8-rectangle.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-r = Rectangle(3, 5)
 
-print(r)
-print(dir(r))
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
 
-try:
-    print("Rectangle: {} - {}".format(r.width, r.height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    r2 = Rectangle(4, True)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+    def __init__(self, width, height):
+        """Intialize a new Rectangle.
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
