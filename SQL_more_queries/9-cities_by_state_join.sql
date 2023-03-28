2f912986ce1328458225d0aeccbb485089cdfc1f
mathieumunyakazimoise <m.munyakazi1@alustudent.com>
2023-03-28 05:22:07 -0700

    hshd

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
--- 8-cities_of_california_subquery.sql
-SELECT cities.id, cities.name
+-- 9-cities_by_state_join.sql
+SELECT cities.id, cities.name, states.name
 FROM cities, states
 WHERE cities.state_id = states.id
-    AND states.name = 'California'
-    ORDER BY cities.id ASC;
+ORDER BY cities.id ASC;
