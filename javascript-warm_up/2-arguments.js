2d3167b1f3b8226f2db55c814347df07f4c83cc9
mathieumunyakazimoise <124375301+mathieumunyakazimoise@users.noreply.github.com>
2023-06-05 11:46:24 +0000

    vv

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 switch (process.argv.length) {
-  case 2:
-    console.log('No argument');
-    break;
-  case 3:
-    console.log('Argument found');
-    break;
-  default:
-    console.log('Arguments found');
-    break;
-}
-
+    case 2:
+      console.log('No argument');
+      break;
+    case 3:
+      console.log('Argument found');
+      break;
+    default:
+      console.log('Arguments found');
+      break;
+  }
   
