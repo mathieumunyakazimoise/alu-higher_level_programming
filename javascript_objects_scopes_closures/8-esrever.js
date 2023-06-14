176b5286b04cc07089cca5d4c830dacc6384aa4c
mathieumunyakazimoise <124375301+mathieumunyakazimoise@users.noreply.github.com>
2023-06-14 16:28:28 +0000

    mm

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-const size = parseInt(process.argv[2]);
-
-if (isNaN(size)) {
-  console.log('Missing size');
-} else {
-  for (let i = 0; i < size; i++) {
-    let row = '';
-    for (let j = 0; j < size; j++) {
-      row += 'X';
-    }
-    console.log(row);
+exports.esrever = function (list = []) {
+  const newList = [];
+  for (let i = list.length; i >= 0; i--) {
+    newList.push(list[i]);
   }
-}
+  newList.shift();
+  return newList;
+};
